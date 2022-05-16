b58f4118f2b87fdace6eaabf1fc5629b7ab026cd
Aman Thakur <builtbyaman@gmail.com>
2022-05-16 14:21:00 +0530

    updating badge every sec

--- popup.js
+++ popup.js
@@ -3,15 +3,6 @@ const nameElement = document.getElementById('name');
 const currentTime = new Date().toLocaleTimeString();
 timeElement.textContent = `The time is ${currentTime}`;
 
-chrome.action.setBadgeText(
-  {
-    text: 'TIME',
-  },
-  () => {
-    console.log('Set badge');
-  }
-);
-
 chrome.storage.sync.get(['name'], (res) => {
   if (res.name) nameElement.textContent = `Your name is: ${res.name}`;
 });
