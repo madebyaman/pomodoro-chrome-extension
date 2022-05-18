3c736bdc634dcc576b5f2b11d76d5fc8db247e71
Aman Thakur <builtbyaman@gmail.com>
2022-05-18 12:21:00 +0530

    alarms

--- popup.js
+++ popup.js
@@ -1,7 +1,21 @@
 const timeElement = document.getElementById('time');
 const nameElement = document.getElementById('name');
-const currentTime = new Date().toLocaleTimeString();
-timeElement.textContent = `The time is ${currentTime}`;
+const timerElement = document.getElementById('timer');
+
+function updateTimeElement() {
+  chrome.storage.local.get(['timer'], (res) => {
+    const time = res.timer ?? 0;
+    timerElement.textContent = time;
+  });
+
+  const currentTime = new Date().toLocaleTimeString();
+  timeElement.textContent = `The time is ${currentTime}`;
+}
+
+updateTimeElement();
+setInterval(() => {
+  updateTimeElement();
+}, 1000);
 
 chrome.storage.sync.get(['name'], (res) => {
   if (res.name) nameElement.textContent = `Your name is: ${res.name}`;
